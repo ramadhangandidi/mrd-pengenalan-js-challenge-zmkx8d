730d82ddaac5bec2de896f8d92a2021f3124dded
ramadhangandidi <47000265+ramadhangandidi@users.noreply.github.com>
2022-11-14 22:45:38 +0700

    1 file modified

--- index.js
+++ index.js
@@ -5,46 +5,48 @@ function kalkulasiNilaiTotal(uts, uas, tugas) {
   const nilaiUts = uts;
 
   // TODO: 2. set nilaiUas dari parameter `uas`
-  const nilaiUas = uas
+  const nilaiUas = uas;
 
   // TODO: 3. set nilaiTugas dari parameter `tugas`
-  const nilaiTugas;
+  const nilaiTugas = tugas;
 
   // TODO: 4. lakukan perhitungan di sini
   // nilaiTotal didapat dari penjumlahan sebagai berikut
   // - 30% dari nilai UTS
   // - 30% dari nilai Tugas
   // - 40% dari nilai UAS
-  const nilaiTotal;
+  const nilaiTotal = 0.3 * nilaiUts + 0.3 * nilaiTugas + 0.4 * nilaiUas;
 
   // JANGAN DIMODIFIKASI
   return nilaiTotal;
-};
+}
 
 function cetakHasil(total) {
   // TODO: 5. set nilaiTotal dari parameter `total`
-  const nilaiTotal;
-  
+  const nilaiTotal = total;
+
   // TODO: 6. dengan menggunakan relational operator
   // buatlah pernyataan apakah orang ini dianggap lulus atau tidak
   // dilihat dari apakah nilaiTotal lebih besar sama dengan 70
-  const apakahLulus;
+  const apakahLulus = nilaiTotal >= 70;
 
   // Bagian ini tidak DIMODIFIKASI
   let kalimatHasil;
 
   // Cara penulisan ini namanya adalah `pernyataan kondisi` (conditional statement)
   // akan dipelajari nanti yah !
-  if(apakahLulus) {
+  if (apakahLulus) {
     // TODO: 7. tuliskan hasil akhir - berhasil lulus
     // Ganti kalimatHasil jadi [Selamat ! Anda lulus dengan nilai <nilaiTotal>.]
 
-    console.log(kalimatHasil);
+    console.log('Selamat ! Anda lulus dengan nilai ' + nilaiTotal);
   } else {
     // TODO: 8. tuliskan hasil akhir - tidak lulus
     // Ganti kalimatHasil jadi [Mohon maaf... Anda dinyatakan tidak lulus karena nilai Anda kurang dari 70.]
-    
-    console.log(kalimatHasil);
+
+    console.log(
+      'Mohon maaf... Anda dinyatakan tidak lulus karena nilai Anda kurang dari 70.'
+    );
   }
 }
 
@@ -52,12 +54,12 @@ function cetakHasil(total) {
 // JANGAN DIMODIFIKASI
 function main() {
   // Anak 1: Lulus
-  let nilaiAnakPertama = kalkulasiNilaiTotal(80,70,70);
+  let nilaiAnakPertama = kalkulasiNilaiTotal(80, 70, 70);
   cetakHasil(nilaiAnakPertama);
   // Output: Selamat ! Anda lulus dengan nilai 73.
 
   // Anak 2: Tidak Lulus
-  let nilaiAnakKedua = kalkulasiNilaiTotal(70,60,70);
+  let nilaiAnakKedua = kalkulasiNilaiTotal(70, 60, 70);
   cetakHasil(nilaiAnakKedua);
   // Output: Mohon maaf... Anda dinyatakan tidak lulus karena nilai Anda kurang dari 70.
 }
